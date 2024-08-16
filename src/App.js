d659efda51953b4e8926a62a8a55623f5caf6072
Edona Rexhaj <131316928+ShiineCodes@users.noreply.github.com>
2024-08-16 23:26:08 +0200

    Search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import books from './books.jpg';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className='App'>
+      <div className='container'>
       <header className='App-header'>
         <img src={books} className='App-books img-fluid' alt='books' />
       </header>
+      <main>
+      <Dictionary/>
+
+
+      </main>
+      <footer className='App-footer'> Coded by Edona Rexhaj</footer>
+      </div>
     </div>
   );
 }
