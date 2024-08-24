d903d77248069745d518f809f680b8e250c50c11
Edona Rexhaj <131316928+ShiineCodes@users.noreply.github.com>
2024-08-24 23:11:05 +0200

    Created photogallery, added new API for photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,34 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import Results from './Results';
+import Photos from './Photos';
 import './Dictionary.css';
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      '1fpea4yZAaCiqDq5RMkLu954qGfcH89VsruO1xsYYkUzoLceudRzLtTr';
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9;`;
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -37,13 +49,18 @@ export default function Dictionary(props) {
         <section>
           <h1>What word do you want to look up?</h1>
           <form onSubmit={handleSubmit}>
-            <input type='search' onChange={handleKeywordChange} defaultValue={props.defaultKeyword} />
+            <input
+              type='search'
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
           </form>
           <div className='hint'>
             suggested words: sunset, wine, love, forest...
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
