dbf2cb23cde807666ef097ba0e3a7106b682b9bf
Edona Rexhaj <131316928+ShiineCodes@users.noreply.github.com>
2024-08-24 23:36:25 +0200

    Changed styling on photogallery and footer, and link to github and netlify

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
 import books from './books.png';
-import Dictionary from "./Dictionary";
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className='App'>
       <div className='container'>
-      <header className='App-header'>
-        <img src={books} className='App-books img-fluid' alt='books' />
-      </header>
-      <main>
-      <Dictionary defaultKeyword="mountain"/>
-
-
-      </main>
-      <footer className='App-footer'> Coded by Edona Rexhaj </footer>
+        <header className='App-header'>
+          <img src={books} className='App-books img-fluid' alt='books' />
+        </header>
+        <main>
+          <Dictionary defaultKeyword='mountain' />
+        </main>
+        <footer className='App-footer'>
+          {' '}
+          Coded by Edona Rexhaj and is open-sourced on{' '}
+          <a href='https://github.com/ShiineCodes/dictionary-project'>
+            Github
+          </a>{' '}
+          and hosted on{' '}
+          <a href='https://main--keen-concha-0847f2.netlify.app/'>Netlify</a>{' '}
+        </footer>
       </div>
     </div>
   );
 }
-
